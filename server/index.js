e06f9e6b7b9672c242ed20efeb2866cd5b193452
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-08-05 11:33:51 +0530

    Handling CORS

--- server/index.js
+++ server/index.js
@@ -23,16 +23,27 @@ app.use(session({
     secret: 'Session started'
   }));
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+  // Allow requests from specific origins
+const allowedOrigins = ['https://naveen-poc-stripe.vercel.app/', 'http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
