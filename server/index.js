2a34dd980b71225e109411c046411c6bd49219cf
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-08-05 11:51:21 +0530

    CORS error .3

--- server/index.js
+++ server/index.js
@@ -23,27 +23,35 @@ app.use(session({
     secret: 'Session started'
   }));
 
-  // Allow requests from specific origins
-const allowedOrigins = ['https://naveen-poc-stripe.vercel.app/', 'http://localhost:3000'];
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+  app.use(function (req, res, next) {
 
-app.use(cors(corsOptions));
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
-// app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
