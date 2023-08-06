40c8bde7ccff678e1d32f834cc04220d13227a03
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-08-06 14:16:35 +0530

    CORS Debug 10

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use("/stripe", Stripe);
   
 
 app.get("/", (req, res) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   // res.setHeader('Access-Control-Allow-Credentials', true);
